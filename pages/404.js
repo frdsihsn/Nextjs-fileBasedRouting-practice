9416f697a3bee2c087be6907c314384a08be670f
Firdaus Ihsan <firdaus.b@affinidi.com>
2022-08-28 14:37:10 +0800

    file-based routing practice

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+    return(
+        <div>
+            <h1>Page Not Found Woi</h1>
+        </div>
+    )
+}
+
+export default NotFoundPage;
---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+
+function ClientProjectsPage() {
+    const router = useRouter();
+
+    function loadProjectHandler() {
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id: 'max', clientprojectid: 'projectA'},
+        })
+    }
+    return (
+        <div>
+            <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
+        </div>
+    )
+}
+
+export default ClientProjectsPage;
